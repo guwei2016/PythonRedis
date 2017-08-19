55ba32d717d1ba8670f9c9361be5fc8c32bcc30d
guwei <guwei@saicstack.com>
2017-08-19 13:55:45 +0800

    201708191355

---
+++ Redis-Connect.py
@@ -0,0 +1,12 @@
+import redis
+r=redis.StrictRedis(host='139.196.95.33',port=7001,db=0,password='guwei123456',decode_responses=True)
+UserName=r.get('name')
+Color=r.hget('car','color')
+print(Color)
+#if bytes.decode(UserName)=='guwei':
+if UserName =='guwei':
+    print(UserName)
+    print('Connect OK!')
+All_Keys=r.keys()
+for Key in All_Keys:
+    print(Key)
